713cc533ea8810fc74ad8204414515cd76efc512
xilareyna <68404722+xilareyna@users.noreply.github.com>
2021-01-10 13:51:12 -0600

    CARMEN SANDIEGO - Halfway there! Hot on her trail.

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -25,16 +25,20 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+-- SELECT name FROM country WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1; 
+
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
-
-
+--SELECT code FROM country WHERE name = 'Holy See (Vatican City State)';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+--SELECT * FROM countrylanguage WHERE language='Italian' ORDER BY percentage DESC;
+--SELECT name FROM country WHERE code = 'SMR';
 
 
+--SELECT "language" FROM countrylanguage WHERE countrycode = 'VAT';
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
