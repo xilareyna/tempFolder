3eaf6af86186031695e6d477f2d9d8a2c95e35f9
xilareyna <68404722+xilareyna@users.noreply.github.com>
2021-01-10 14:54:16 -0600

    Commit: CARMEN SANDIEGO - I found Carmen Sandiego

--- carmen-sandiego/find_carmen.sql
+++ carmen-sandiego/find_carmen.sql
@@ -43,17 +43,18 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
-
+--SELECT name FROM city WHERE countrycode = 'SMR';
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-
+--SELECT * FROM country WHERE id = 211;
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
-
+--SELECT * FROM city WHERE name LIKE 'Serra';
+--SELECT * FROM country WHERE code = 'BRA';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -70,7 +71,7 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+--SELECT * FROM city WHERE population = '91084';
 
 
-
--- She's in ____________________________!
+-- She's in ________Santa Monica____________________!
